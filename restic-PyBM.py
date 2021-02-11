100e8aca0e7e84b8b6083ddad21a9d9d34b665ec
Eric Viseur <eric.viseur@gmail.com>
2021-02-11 20:48:48 +0100

    Typo

--- restic-PyBM.py
+++ restic-PyBM.py
@@ -189,7 +189,7 @@ for currentRepo in reposToProcess:
       # Create a new restic repo with the infos provided in backup.yml
       command = resticLocation + ' init --repo ' + repos[currentRepo]['location']
       if duplicate in repos[currentRepo].keys():
-        command += ' --repo2 ' + repos[currentRepo['duplicate']]['location'] '--copy-chunker-params'
+        command += ' --repo2 ' + repos[currentRepo['duplicate']]['location'] + '--copy-chunker-params'
         print(command)
         exit(0)
       result = run_command(command, commandEnv)
